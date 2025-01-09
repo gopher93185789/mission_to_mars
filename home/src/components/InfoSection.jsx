bfe7d6164459cb0b4b1db38efa80b5bfde8cda7c
yn1243 <102717@glr.nl>
2025-01-09 14:39:13 +0000

    addedd info component

--- home/src/components/InfoSection.jsx
+++ home/src/components/InfoSection.jsx
@@ -1,21 +1,20 @@
 const keys = [
-    {itemname: "galaxy", info: "Milky Way"},
-    {itemname: "galaxy", info: "Milky Way"},
-    {itemname: "galaxy", info: "Milky Way"},
-    {itemname: "galaxy", info: "Milky Way"},
-    {itemname: "galaxy", info: "Milky Way"},
-]
+    { itemname: "Galaxy", info: "Milky Way" },             
+    { itemname: "Diameter", info: "6,779 km" },            
+    { itemname: "Day length", info: "24.6 hours" },        
+    { itemname: "Temperature", info: "-60°C (average)" },   
+    { itemname: "Climate", info: "Cold, desert-like" },     
+];
 
-export default function InfoSection() {
+export function InfoSection() {
     return (    
-        <div className="h-48 w-10/12 bg-neutral-600 flex flex-row gap-5">
+        <div className="h-48 w-8/12 flex flex-row gap-5 overflow-hidden">
             {keys.map((item, idx) => (
-                <div key={idx} className="h-full w-full bg-neutral-300 flex flex-col items-center" >
-                    <p className="text-white w-full text-center">{item.itemname}</p>
-                    <p className="text-white w-full text-center">{item.info}</p>
+                <div key={idx} className="h-full w-96 flex flex-col justify-center p-5 items-center" >
+                    <p className="text-[#BDC5E2] text-4xl w-full text-nowrap font-bold text-opacity-70">{item.itemname}</p>
+                    <p className="text-white text-3xl text-opacity-90 w-full text-nowrap">{item.info}</p>
                 </div>
             ))}
         </div>
     )
-    
 }
