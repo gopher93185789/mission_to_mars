23f4f6f17eccbd22ed4cf7b408a57b9407f3234c
yn1243 <102717@glr.nl>
2025-01-09 15:57:26 +0000

    style updates

--- home/src/components/Navbar.jsx
+++ home/src/components/Navbar.jsx
@@ -26,10 +26,12 @@ export function Navbar() {
     }, [click])
     
     return (
-        <div className="gap-12 h-28 flex justify-center flex-row">
+        <div className="gap-12 h-28 flex justify-center select-none flex-row">
             {keys.map((item, idx) => (
                 <div className="w-full flex justify-center items-center" key={idx}>
-                    <p onClick={() => {setClick(item.itemname)}} className={`w-full text-xl text-center text-white hover:text-opacity-100 text-opacity-50 duration-300 ease-in-out hover:cursor-pointer ${click == item.itemname ? "scale-95": "hover:scale-105"}`}><a href={item.link}>{item.itemname}</a></p>
+                    <p onClick={() => {setClick(item.itemname)}} className={`w-full text-xl text-center text-white hover:text-opacity-100 text-opacity-50 duration-300 ease-in-out hover:cursor-pointer ${click == item.itemname ? "scale-95": ""}`}>
+                        <a href={item.link}>{item.itemname}</a>
+                    </p>
                 </div>
             ))}
         </div>
