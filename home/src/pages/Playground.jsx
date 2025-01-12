4b7772556185882a1e86268964cc5b1431b87444
script kiddie <anish.chittu2007@gmail.com>
2025-01-12 23:24:09 +0100

    feat: add keyboard interaction to clear model selection and improve button styling

--- home/src/pages/Playground.jsx
+++ home/src/pages/Playground.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 
@@ -6,7 +6,6 @@ import { OrbitControls, useGLTF } from '@react-three/drei';
 function RotatingModel({model = 'models/mars.glb'}) {
   const modelRef = useRef();
   const { scene } = useGLTF(model);  
-
 
 
   return (
@@ -19,17 +18,31 @@ function RotatingModel({model = 'models/mars.glb'}) {
 export function Playground() {
     const [model, setModel] = useState('')
 
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            if (event.key === 'Escape'){
+                setModel('')
+            };
+            console.log(`Key pressed: ${event.key}`);
+        };
+
+        window.addEventListener("keydown", handleKeyPress);
+                
+        return () => {
+            window.removeEventListener("keydown", handleKeyPress);
+        };
+    }, []); 
+
     return (
         <>
             <div className="w-full  h-fit mt-8 flex flex-row justify-center items-center">
-                <button onClick={() => setModel('models/ship.glb')} className="text-white w-32 bg-neutral-900 text-xl font-bold text-opacity-50 hover:text-opacity-100 active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50  rounded-l-xl ">ship</button>
-               
-                <button onClick={() => setModel('models/mars.glb')} className="text-white w-32 bg-neutral-900 text-xl font-bold text-opacity-50 hover:text-opacity-100 active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50">mars</button>
-                <button onClick={() => setModel('models/base.glb')} className="text-white w-32 bg-neutral-900 text-xl font-bold text-opacity-50 hover:text-opacity-100 active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50  rounded-r-xl ">base</button>
+                <button onClick={() => setModel('models/ship.glb')} className={`text-white w-32 bg-neutral-900 text-xl font-bold ${model === 'models/ship.glb' ? "text-opacity-100 border-opacity-100" : "text-opacity-50 hover:text-opacity-100"}   active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50  rounded-l-xl `}>ship</button>
+                <button onClick={() => setModel('models/mars.glb')} className={`text-white w-32 bg-neutral-900 text-xl font-bold ${model === 'models/mars.glb' ? "text-opacity-100 border-opacity-100" : "text-opacity-50 hover:text-opacity-100"}  active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50`}>mars</button>
+                <button onClick={() => setModel('models/base.glb')} className={`text-white w-32 bg-neutral-900 text-xl font-bold ${model === 'models/base.glb' ? "text-opacity-100 border-opacity-100" : "text-opacity-50 hover:text-opacity-100"}  active:scale-95 hover:border-opacity-100 duration-300 ease-in-out py-5 border border-white border-opacity-50  rounded-r-xl `}>base</button>
             </div>
 
             {model != ''?             
-            <Canvas className='mt-4 rounded-t-3xl' camera={{ position: [0, 0, 5], zoom: 1.2 }}>
+            <Canvas className='mt-4 rounded-t-3xl hover:cursor-move' camera={{ position: [0, 0, 5], zoom: 1.2 }}>
                 <ambientLight intensity={1.5} />
                 <pointLight position={[10, 10, 10]} />
                 <RotatingModel model={model} position={[0, 0, 0]}/>
