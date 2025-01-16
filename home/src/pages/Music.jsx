e4a947830b141e9c1f9e666a210dd3ff416e0f80
yn1243 <102717@glr.nl>
2025-01-16 17:12:10 +0100

    Update Music.jsx

--- home/src/pages/Music.jsx
+++ home/src/pages/Music.jsx
@@ -63,10 +63,6 @@ export function Music() {
     }
 
     useEffect(() => {
-        console.log(playing)
-    }, [playing])
-
-    useEffect(() => {
         if (playingPath) {
             audioRef.current.load();
             if (play) {
