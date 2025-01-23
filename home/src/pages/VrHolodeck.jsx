ae4e662c3eee04ce8b780351610271af4c4277f5
yn1243 <102717@glr.nl>
2025-01-23 13:26:54 +0000

    added info section to vr deck

--- home/src/pages/VrHolodeck.jsx
+++ home/src/pages/VrHolodeck.jsx
@@ -29,6 +29,15 @@ export function VrHolodeck() {
               <OrbitControls enablePan={false}  maxPolarAngle={1.7} maxDistance={7} minDistance={7} rotateSpeed={0.5} />
           </Canvas> 
         </div>
+
+        <button className='absolute top-0 left-0 group bg-neutral-900 z-10 rounded-full size-16 text-2xl font-bold text-white ml-5 mt-5'>
+            ?
+            <div className='w-96 absolute p-3 min-h-20 scale-y-0 group-hover:scale-y-100 duration-500 ease-in-out origin-top bg-neutral-900 mt-5 rounded-lg'>
+                <p className='text-opacity-50 text-left text-white text-sm'>
+                Een VR holodeck is een virtuele ruimte waar gebruikers volledig kunnen onderdompelen in een digitale wereld. Met behulp van virtual reality technologieën kunnen ze 3D omgevingen verkennen, interactieve objecten manipuleren en met anderen communiceren alsof ze fysiek aanwezig zijn. Het biedt een unieke, meeslepende ervaring die de grenzen tussen de fysieke en digitale wereld vervaagt. Cool, toch?
+                </p>
+            </div>
+        </button>
         </>
     )
 }
